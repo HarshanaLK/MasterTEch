91a01eb15a480c71e49eb956ef9748e16f83f22a
Harshana Karunarathna <95669599+HarshanaLK@users.noreply.github.com>
2024-06-05 15:43:34 +0530

    Update file

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,38 @@
-import React,{} from 'react'
-import Slide from '../Elements/Slide'
-import DisCard from '../Elements/DisCard'
-import ProDivC from '../Elements/ProDivC'
-import ProductsC from '../Elements/ProductsC'
+import React from 'react';
+import Slide from '../Elements/Slide';
+import ProDivC from '../Elements/ProDivC';
+import ProductsC from '../Elements/ProductsC';
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
-
+const DisCard = ({ title }) => {
+    return (
+        <Link to={`/Shop/?cg=${title}`} className="cardscg">
+            <div>
+                <h1>{title}</h1>
+            </div>
+        </Link>
+    );
+};
 
 const Home = () => {
- 
     return (
         <>
-        <Helmet>
-            <title>MasterTech</title>
-        </Helmet>
-             <div>
-                <Slide/>
-                 <div className="cards">
-                         <DisCard title='Dekstop'/>
-                         <DisCard title='Laptop'/>
-                         <DisCard title='Accessories'/>                
-                 </div>
-                <ProDivC/>
-                <ProductsC/>
-        </div>
+            <Helmet>
+                <title>MasterTech</title>
+            </Helmet>
+            <div>
+                <Slide />
+                <div className="cards">
+                    <DisCard title='Desktop' />
+                    <DisCard title='Laptop' />
+                    <DisCard title='Accessories' />
+                </div>
+                <ProDivC />
+                <ProductsC />
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
